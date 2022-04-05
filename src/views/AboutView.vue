b319a918c0ec9d32f37621a4b29a603da3172197
marcdevrenan <marcdevrenan@gmail.com>
2022-04-05 08:11:27 -0300

    added about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Instituto Infnet</h1>
+    <h2>AT - Frameworks Front-End e Conexão com Back-End</h2>
+    <h3>Prof(a): Vanessa Cristina Martins</h3>
+    <h3>Aluno: Renan Ferreira Marcílio</h3>
+    <p>Projeto de listagem de tarefas feito com Vue para entrega final da disciplina</p>
   </div>
 </template>
